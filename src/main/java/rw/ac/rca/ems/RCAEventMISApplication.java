33b1e3708937192af139f804aae4cde766dc73a6
irumvanselme <andesanselme@gmail.com>
2022-02-04 12:31:34 +0200

    hehehe

--- src/main/java/rw/ac/rca/ems/RCAEventMISApplication.java
+++ src/main/java/rw/ac/rca/ems/RCAEventMISApplication.java
@@ -2,6 +2,7 @@ package rw.ac.rca.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -9,5 +10,10 @@ public class RCAEventMISApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RCAEventMISApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RCAEventMISApplication.class);
     }
 }
